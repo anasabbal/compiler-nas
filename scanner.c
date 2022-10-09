98a21f4f4975a2bc7d5bc8a6cca7c786542b939a
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-10-09 13:54:51 +0100

    begin scanner part (#01)

---
+++ scanner.c
@@ -0,0 +1,58 @@
+//
+// Created by Anas on 8/10/2022.
+//
+
+
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+#include "scanner.h"
+
+typedef struct {
+    const char *start;
+    const char *current;
+    int line;
+}Scanner;
+
+Scanner  scanner;
+
+void initScanner(const char *source){
+    scanner.start = source;
+    scanner.current = source;
+    scanner.line = 1;
+}
+static bool isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           c == '_';
+}
+
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+static char peek() {
+    return *scanner.current;
+}
+
+static char peekNext() {
+    if (isAtEnd()) return '\0';
+    return scanner.current[1];
+}
+
+static bool match(char expected) {
+    if (isAtEnd()) return false;
+    if (*scanner.current != expected) return false;
+    scanner.current++;
+    return true;
+}
